9664441a946bcd7989fa9141e94907be4d3efb8a
guerlab <master@guerlab.net>
2020-02-05 14:09:57 +0800

    更新swagger聚合服务

--- smart-platform-basic/smart-platform-basic-swagger/docker/build.sh
+++ smart-platform-basic/smart-platform-basic-swagger/docker/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cp ../target/smart-platform-basic-swagger-0.1.0-exec.jar app.jar
+cp ../target/smart-platform-basic-swagger-exec.jar app.jar
 docker rmi -f registry.cn-shenzhen.aliyuncs.com/hzly/smart_platform_basic_swagger
 docker build -t registry.cn-shenzhen.aliyuncs.com/hzly/smart_platform_basic_swagger .
 docker push registry.cn-shenzhen.aliyuncs.com/hzly/smart_platform_basic_swagger
