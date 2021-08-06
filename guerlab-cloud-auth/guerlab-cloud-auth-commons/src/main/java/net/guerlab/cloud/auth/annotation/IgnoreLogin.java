2c81052da6e809494adc4fda511a472fa2215c1d
guerlab <master@guerlab.net>
2021-08-06 09:02:55 +0800

    调整注解所在的类路径

--- guerlab-cloud-auth/guerlab-cloud-auth-commons/src/main/java/net/guerlab/cloud/auth/annotation/IgnoreLogin.java
+++ guerlab-cloud-auth/guerlab-cloud-auth-commons/src/main/java/net/guerlab/cloud/auth/annotation/IgnoreLogin.java
@@ -10,7 +10,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package net.guerlab.cloud.auth.web.annotation;
+package net.guerlab.cloud.auth.annotation;
 
 import java.lang.annotation.*;
 
