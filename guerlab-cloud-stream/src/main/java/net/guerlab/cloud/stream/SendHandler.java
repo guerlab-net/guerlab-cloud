90c4e74d28535c286df6e59a38ef480bb0896160
guerlab <master@guerlab.net>
2021-08-11 10:45:59 +0800

    增强stream模块以适应spring cloud stream 3.1之后的模型

---
+++ guerlab-cloud-stream/src/main/java/net/guerlab/cloud/stream/SendHandler.java
@@ -0,0 +1,90 @@
+package net.guerlab.cloud.stream;
+
+import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+
+import java.util.Objects;
+
+/**
+ * 发送处理
+ *
+ * @param <T>
+ *         待发送内容类型
+ * @author guer
+ */
+@SuppressWarnings("unused")
+public class SendHandler<T> {
+
+    protected String bindingName;
+
+    protected StreamBridge streamBridge;
+
+    protected MessageChannel messageChannel;
+
+    /**
+     * 创建发送处理
+     *
+     * @param bindingName
+     *         bindingName
+     * @param streamBridge
+     *         streamBridge
+     */
+    public SendHandler(String bindingName, StreamBridge streamBridge) {
+        Objects.requireNonNull(bindingName, "bindingName can not be null");
+        Objects.requireNonNull(streamBridge, "streamBridge can not be null");
+        this.bindingName = formatBindingName(bindingName);
+        this.streamBridge = streamBridge;
+    }
+
+    /**
+     * 创建发送处理
+     *
+     * @param messageChannel
+     *         messageChannel
+     */
+    public SendHandler(MessageChannel messageChannel) {
+        Objects.requireNonNull(messageChannel, "messageChannel can not be null");
+        this.messageChannel = messageChannel;
+    }
+
+    /**
+     * 格式化bindingName
+     *
+     * @param bindingName
+     *         bindingName
+     * @return 格式化后的bindingName
+     */
+    private static String formatBindingName(String bindingName) {
+        if (bindingName.contains(BindingNameConstants.OUT_PARAM_SUFFIX)) {
+            return bindingName;
+        } else {
+            return bindingName + BindingNameConstants.DEFAULT_OUT_PARAM_SUFFIX;
+        }
+    }
+
+    /**
+     * 发送
+     *
+     * @param payload
+     *         待发送内容
+     * @return 是否发送成功
+     */
+    public boolean send(T payload) {
+        Objects.requireNonNull(payload, "payload can not be null");
+
+        if (messageChannel != null) {
+            if (payload instanceof Message<?>) {
+                return messageChannel.send((Message<?>) payload);
+            } else {
+                return messageChannel.send(MessageUtils.toBuilder(payload).build());
+            }
+        } else {
+            if (payload instanceof Message<?>) {
+                return streamBridge.send(bindingName, payload);
+            } else {
+                return streamBridge.send(bindingName, MessageUtils.toBuilder(payload).build());
+            }
+        }
+    }
+}
