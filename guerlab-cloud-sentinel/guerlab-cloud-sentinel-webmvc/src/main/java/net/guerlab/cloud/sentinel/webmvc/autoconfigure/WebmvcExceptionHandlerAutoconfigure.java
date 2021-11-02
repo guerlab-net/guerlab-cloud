f0d0a698ad4d4321d01f2ef770f30f24adf91be8
guerlab <master@guerlab.net>
2021-11-02 09:38:58 +0800

    更新版权信息

--- guerlab-cloud-sentinel/guerlab-cloud-sentinel-webmvc/src/main/java/net/guerlab/cloud/sentinel/webmvc/autoconfigure/WebmvcExceptionHandlerAutoconfigure.java
+++ guerlab-cloud-sentinel/guerlab-cloud-sentinel-webmvc/src/main/java/net/guerlab/cloud/sentinel/webmvc/autoconfigure/WebmvcExceptionHandlerAutoconfigure.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2018-2021 guerlab.net and other contributors.
+ * Copyright 2018-2022 guerlab.net and other contributors.
  *
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      https://www.apache.org/licenses/LICENSE-2.0
+ *      https://www.gnu.org/licenses/lgpl-3.0.html
  *
  * See the License for the specific language governing permissions and
  * limitations under the License.
