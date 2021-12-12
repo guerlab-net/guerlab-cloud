71353f82ca1d6e01ff9907de4e4412b42d447f97
guerlab <master@guerlab.net>
2021-12-12 23:57:20 +0800

    refactor: 更新exportExcel方法，去掉对servlet的依赖，增加ResponseEntity的结果输出，以便于支持webflux环境

---
+++ guerlab-cloud-excel/src/main/java/net/guerlab/cloud/excel/enums/ExcelType.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2018-2022 guerlab.net and other contributors.
+ *
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.gnu.org/licenses/lgpl-3.0.html
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.guerlab.cloud.excel.enums;
+
+/**
+ * excel类型
+ *
+ * @author guer
+ */
+public enum ExcelType {
+
+    /**
+     * XLSX
+     */
+    XLSX(".xlsx"),
+
+    /**
+     * XLS
+     */
+    XLS(".xls");
+
+    private final String suffix;
+
+    ExcelType(String suffix) {
+        this.suffix = suffix;
+    }
+
+    /**
+     * 获取后缀
+     *
+     * @return 后缀
+     */
+    public String getSuffix() {
+        return suffix;
+    }
+}
