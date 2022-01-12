d2bb2802eddf21bff8f97052536a4843c7829963
guerlab <master@guerlab.net>
2022-01-12 23:24:42 +0800

    feat: 增加请求ip解析处理

---
+++ guerlab-cloud-web/guerlab-cloud-web-webmvc/src/main/java/net/guerlab/cloud/web/webmvc/autoconfigure/RequestIpHandlerMethodArgumentResolverAutoConfigure.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2018-2022 guerlab.net and other contributors.
+ *
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.gnu.org/licenses/lgpl-3.0.html
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.guerlab.cloud.web.webmvc.autoconfigure;
+
+import lombok.extern.slf4j.Slf4j;
+import net.guerlab.cloud.web.webmvc.support.WebMvcRequestIpHandlerMethodArgumentResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+/**
+ * 构造请求IP参数处理参数解析自动配置
+ *
+ * @author guer
+ */
+@Slf4j
+@Configuration
+public class RequestIpHandlerMethodArgumentResolverAutoConfigure implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new WebMvcRequestIpHandlerMethodArgumentResolver());
+        log.debug("add WebMvcRequestIpHandlerMethodArgumentResolver");
+    }
+}
+
---
+++ guerlab-cloud-web/guerlab-cloud-web-webmvc/src/main/java/net/guerlab/cloud/web/webmvc/support/WebMvcRequestIpHandlerMethodArgumentResolver.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2018-2022 guerlab.net and other contributors.
+ *
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.gnu.org/licenses/lgpl-3.0.html
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.guerlab.cloud.web.webmvc.support;
+
+import lombok.extern.slf4j.Slf4j;
+import net.guerlab.cloud.commons.ip.IpUtils;
+import net.guerlab.cloud.web.core.annotation.RequestIp;
+import org.springframework.core.MethodParameter;
+import org.springframework.lang.Nullable;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+/**
+ * 请求IP参数处理
+ *
+ * @author guer
+ */
+@Slf4j
+public class WebMvcRequestIpHandlerMethodArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterAnnotation(RequestIp.class) != null;
+    }
+
+    @Nullable
+    @Override
+    public Object resolveArgument(MethodParameter parameter, @Nullable ModelAndViewContainer mavContainer,
+            NativeWebRequest webRequest, @Nullable WebDataBinderFactory binderFactory) {
+        Object nativeRequest = webRequest.getNativeRequest();
+        log.debug("nativeRequest: {}", nativeRequest);
+        String ip = IpUtils.getIp(nativeRequest);
+        log.debug("get ip in nativeRequest: {}", ip);
+        return ip;
+    }
+}
