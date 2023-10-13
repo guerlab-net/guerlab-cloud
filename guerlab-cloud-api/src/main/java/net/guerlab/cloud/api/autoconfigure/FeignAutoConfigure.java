b156605a06adf768e96d260d954445186f3a1de2
guerlab <master@guerlab.net>
2023-10-13 13:55:40 +0800

    更新版权信息

--- guerlab-cloud-api/src/main/java/net/guerlab/cloud/api/autoconfigure/FeignAutoConfigure.java
+++ guerlab-cloud-api/src/main/java/net/guerlab/cloud/api/autoconfigure/FeignAutoConfigure.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2023 guerlab.net and other contributors.
+ * Copyright 2018-2024 guerlab.net and other contributors.
  *
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3 (the "License");
  * you may not use this file except in compliance with the License.
