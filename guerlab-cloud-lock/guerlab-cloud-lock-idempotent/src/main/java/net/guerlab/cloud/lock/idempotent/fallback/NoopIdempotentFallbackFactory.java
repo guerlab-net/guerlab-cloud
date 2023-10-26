3b6681c0f4b3c6adbd7faa00e9ad03f00bcfe818
guerlab <master@guerlab.net>
2023-10-26 14:01:37 +0800

    锁相关失效的包结构调整

---
+++ guerlab-cloud-lock/guerlab-cloud-lock-idempotent/src/main/java/net/guerlab/cloud/lock/idempotent/fallback/NoopIdempotentFallbackFactory.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2018-2024 guerlab.net and other contributors.
+ *
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.gnu.org/licenses/lgpl-3.0.html
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.guerlab.cloud.lock.idempotent.fallback;
+
+import net.guerlab.cloud.lock.core.fallback.NoopFallbackFactory;
+
+/**
+ * 处理幂等快速失败处理工厂的无操作实现.
+ *
+ * @author guer
+ */
+public final class NoopIdempotentFallbackFactory extends NoopFallbackFactory implements IdempotentFallbackFactory { }
