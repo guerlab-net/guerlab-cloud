b46ea2a06f2800b8358076b1de4803363f23e052
guerlab <master@guerlab.net>
2024-02-27 13:48:47 +0800

    mybatis内部异常信息构建优化

--- guerlab-cloud-server/guerlab-cloud-server-orm/src/main/java/net/guerlab/cloud/server/mybatis/exception/handler/builder/MybatisSystemExceptionResponseBuilder.java
+++ guerlab-cloud-server/guerlab-cloud-server-orm/src/main/java/net/guerlab/cloud/server/mybatis/exception/handler/builder/MybatisSystemExceptionResponseBuilder.java
@@ -13,7 +13,9 @@
 
 package net.guerlab.cloud.server.mybatis.exception.handler.builder;
 
-import org.apache.ibatis.exceptions.TooManyResultsException;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.mybatis.spring.MyBatisSystemException;
 
 import net.guerlab.cloud.commons.exception.handler.AbstractResponseBuilder;
@@ -26,26 +28,52 @@ import net.guerlab.cloud.core.result.Fail;
  */
 public class MybatisSystemExceptionResponseBuilder extends AbstractResponseBuilder {
 
+	private static final Map<Class<? extends Throwable>, Class<? extends AbstractResponseBuilder>> BUILDER_MAP = new HashMap<>();
+
+	/**
+	 * 添加异常对应的异常信息构建器
+	 *
+	 * @param causeClass   异常类型
+	 * @param builderClass 异常信息构建器类型
+	 */
+	public static void addBuilder(Class<? extends Throwable> causeClass, Class<? extends AbstractResponseBuilder> builderClass) {
+		BUILDER_MAP.put(causeClass, builderClass);
+	}
+
 	@Override
 	public boolean accept(Throwable e) {
 		return e instanceof MyBatisSystemException;
 	}
 
 	@Override
-	public Fail<Void> build(Throwable e) {
+	public Fail<?> build(Throwable e) {
 		MyBatisSystemException exception = (MyBatisSystemException) e;
 		Throwable cause = exception.getCause();
-		if (cause instanceof TooManyResultsException) {
-			TooManyResultsExceptionResponseBuilder builder = new TooManyResultsExceptionResponseBuilder();
-			builder.setMessageSource(messageSource);
-			builder.setStackTracesHandler(stackTracesHandler);
 
-			return builder.build(cause);
+		for (Map.Entry<Class<? extends Throwable>, Class<? extends AbstractResponseBuilder>> entry : BUILDER_MAP.entrySet()) {
+			if (entry.getKey().isInstance(cause)) {
+				try {
+					AbstractResponseBuilder builder = entry.getValue().getDeclaredConstructor().newInstance();
+					builder.setMessageSource(messageSource);
+					builder.setStackTracesHandler(stackTracesHandler);
+
+					return builder.build(cause);
+				}
+				catch (Exception ignored) {
+				}
+			}
 		}
 
-		String message = exception.getMessage();
-		if (message == null) {
+		if (cause.getCause() != null) {
+			cause = cause.getCause();
+		}
+
+		String message = null;
+		if (cause != null) {
 			message = cause.getMessage();
+		}
+		if (message == null) {
+			message = exception.getMessage();
 		}
 
 		Fail<Void> fail = new Fail<>(message);
