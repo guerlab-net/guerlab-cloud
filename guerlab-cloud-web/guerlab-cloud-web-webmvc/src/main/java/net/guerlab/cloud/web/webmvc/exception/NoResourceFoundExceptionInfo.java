81f145a3c93810c7cbd26d017d5576e190f59658
guerlab <master@guerlab.net>
2024-08-30 13:47:25 +0800

    release to 2024.10.1
    增加对org.springframework.web.servlet.resource.NoResourceFoundException异常的处理

---
+++ guerlab-cloud-web/guerlab-cloud-web-webmvc/src/main/java/net/guerlab/cloud/web/webmvc/exception/NoResourceFoundExceptionInfo.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2018-2024 guerlab.net and other contributors.
+ *
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.gnu.org/licenses/lgpl-3.0.html
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.guerlab.cloud.web.webmvc.exception;
+
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+import net.guerlab.cloud.core.util.EnvUtils;
+import net.guerlab.cloud.web.core.exception.AbstractI18nInfo;
+import net.guerlab.cloud.web.core.exception.Keys;
+
+/**
+ * 未发现处理程序(404).
+ *
+ * @author guer
+ */
+public class NoResourceFoundExceptionInfo extends AbstractI18nInfo {
+
+	private static final String DEFAULT_MSG = EnvUtils.getEnv("GUERLAB_CLOUD_EXCEPTION_MESSAGE_NOT_FOUND", "请求地址无效[%s %s]");
+
+	private final String method;
+
+	private final String url;
+
+	/**
+	 * 通过NoResourceFoundException初始化.
+	 *
+	 * @param cause NoResourceFoundException
+	 */
+	public NoResourceFoundExceptionInfo(NoResourceFoundException cause) {
+		super(cause, 404);
+		method = cause.getHttpMethod().name();
+		url = cause.getResourcePath();
+	}
+
+	@Override
+	protected String getKey() {
+		return Keys.NO_HANDLER_FOUND;
+	}
+
+	@Override
+	protected Object[] getArgs() {
+		return new Object[] {method, url};
+	}
+
+	@Override
+	protected String getDefaultMessage() {
+		return String.format(DEFAULT_MSG, method, url);
+	}
+}
---
+++ guerlab-cloud-web/guerlab-cloud-web-webmvc/src/main/java/net/guerlab/cloud/web/webmvc/exception/handler/builder/NoResourceFoundExceptionInfoBuilder.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2018-2024 guerlab.net and other contributors.
+ *
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.gnu.org/licenses/lgpl-3.0.html
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.guerlab.cloud.web.webmvc.exception.handler.builder;
+
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+import net.guerlab.cloud.core.result.Fail;
+import net.guerlab.cloud.web.core.exception.handler.AbstractI18nResponseBuilder;
+import net.guerlab.cloud.web.webmvc.exception.NoResourceFoundExceptionInfo;
+
+/**
+ * NoResourceFoundException异常处理.
+ *
+ * @author guer
+ */
+public class NoResourceFoundExceptionInfoBuilder extends AbstractI18nResponseBuilder {
+
+	@Override
+	public boolean accept(Throwable e) {
+		return e instanceof NoResourceFoundException;
+	}
+
+	@Override
+	public Fail<Void> build(Throwable e) {
+		NoResourceFoundException exception = (NoResourceFoundException) e;
+		return buildByI18nInfo(new NoResourceFoundExceptionInfo(exception), e);
+	}
+}
