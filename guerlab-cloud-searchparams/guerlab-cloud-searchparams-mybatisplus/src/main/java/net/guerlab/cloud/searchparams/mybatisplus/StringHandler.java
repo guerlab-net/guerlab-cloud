83255cfce8da7b06100242021b1efe23b26e82de
guerlab <master@guerlab.net>
2024-09-12 10:38:01 +0800

    json查询参数化处理

--- guerlab-cloud-searchparams/guerlab-cloud-searchparams-mybatisplus/src/main/java/net/guerlab/cloud/searchparams/mybatisplus/StringHandler.java
+++ guerlab-cloud-searchparams/guerlab-cloud-searchparams-mybatisplus/src/main/java/net/guerlab/cloud/searchparams/mybatisplus/StringHandler.java
@@ -59,23 +59,23 @@ public class StringHandler extends AbstractMyBatisPlusSearchParamsHandler {
 		if (dbType == DbType.MYSQL) {
 			String sqlTemplate;
 			if (searchModelType == SearchModelType.NOT_IN) {
-				sqlTemplate = "JSON_SEARCH(%s, 'one', '%s', null, '%s') IS NULL";
+				sqlTemplate = "JSON_SEARCH(%s, 'one', '{0}', null, '%s') IS NULL";
 			}
 			else {
-				sqlTemplate = "JSON_SEARCH(%s, 'one', '%s', null, '%s') IS NOT NULL";
+				sqlTemplate = "JSON_SEARCH(%s, 'one', '{0}', null, '%s') IS NOT NULL";
 			}
-			wrapper.apply(String.format(sqlTemplate, columnName, str, jsonPath));
+			wrapper.apply(sqlTemplate.formatted(columnName, jsonPath), str);
 		}
 		else if (dbType == DbType.ORACLE) {
 			String sqlTemplate;
 			if (searchModelType == SearchModelType.NOT_IN) {
-				sqlTemplate = "json_exists(%s, '%s?(!(@ == \"%s\"))')";
+				sqlTemplate = "json_exists(%s, '%s?(!(@ == \"{0}\"))')";
 			}
 			else {
-				sqlTemplate = "json_exists(%s, '%s?(@ == \"%s\")')";
+				sqlTemplate = "json_exists(%s, '%s?(@ == \"{0}\")')";
 			}
 
-			wrapper.apply(String.format(sqlTemplate, columnName, jsonPath, str));
+			wrapper.apply(sqlTemplate.formatted(columnName, jsonPath), str);
 		}
 	}
 
