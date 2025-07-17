00b7868e08040733a5e194c580cc1614278c18cc
guerlab <master@guerlab.net>
2025-07-17 10:22:17 +0800

    feign的header添加增加安全过滤

--- guerlab-cloud-api/src/main/java/net/guerlab/cloud/api/headers/HeadersRequestInterceptor.java
+++ guerlab-cloud-api/src/main/java/net/guerlab/cloud/api/headers/HeadersRequestInterceptor.java
@@ -13,6 +13,9 @@
 
 package net.guerlab.cloud.api.headers;
 
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
 import lombok.extern.slf4j.Slf4j;
@@ -27,6 +30,16 @@ import net.guerlab.cloud.auth.context.AbstractContextHandler;
 @Slf4j
 public class HeadersRequestInterceptor implements RequestInterceptor {
 
+	private static final Pattern ASSIGNED_AND_NOT_ISO_CONTROL_PATTERN = Pattern
+			.compile("[\\p{IsAssigned}&&[^\\p{IsControl}]]*");
+
+	private static final Predicate<String> ASSIGNED_AND_NOT_ISO_CONTROL_PREDICATE = (s) -> ASSIGNED_AND_NOT_ISO_CONTROL_PATTERN.matcher(s)
+			.matches();
+
+	private static final Pattern HEADER_VALUE_PATTERN = Pattern.compile("[\\p{IsAssigned}&&[[^\\p{IsControl}]||\\t]]*");
+
+	private static final Predicate<String> HEADER_VALUE_PREDICATE = (s) -> HEADER_VALUE_PATTERN.matcher(s).matches();
+
 	@Override
 	public void apply(RequestTemplate requestTemplate) {
 		AbstractContextHandler.getAllTransfer().forEach((header, value) -> addHeader(header, value, requestTemplate));
@@ -34,6 +47,14 @@ public class HeadersRequestInterceptor implements RequestInterceptor {
 	}
 
 	private void addHeader(String header, String value, RequestTemplate requestTemplate) {
+		if (!ASSIGNED_AND_NOT_ISO_CONTROL_PREDICATE.test(header)) {
+			log.debug("header not match pattern: {}", header);
+			return;
+		}
+		if (!HEADER_VALUE_PREDICATE.test(value)) {
+			log.debug("header value not match pattern: {} = {}", header, value);
+			return;
+		}
 		log.debug("add header: {} = {}", header, value);
 		requestTemplate.header(header, value);
 	}
